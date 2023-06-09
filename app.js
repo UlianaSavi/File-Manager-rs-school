edfd9e65c71531805dd97e58e2598d4aca4b9cb4
UlianaSavi <uljasha.sav@gmail.com>
2023-06-09 21:16:08 +0600

    feat: create folders strugture & add basic methods into app.js

--- app.js
+++ app.js
@@ -1,27 +1,35 @@
-import readline from 'readline';
+// import readline from 'readline';
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
 
+// rl.question('npm run start -- --username=', name => {
+//     console.log(`Welcome to the File Manager, ${name}!`);
+//     rl.close();
+// });
 
-const init = () => {
-    start(); 
-}
-
-const start = () => {
-    // rl.question('npm run start -- --username=', name => {
-    //     console.log(`Welcome to the File Manager, ${name}!`);
-    //     rl.close();
-    // }); 
+const getUserName = () => {
     const args = process.argv.slice(2);
     const regexp = /(--)\w+/;
     const username = args.find((arg) => arg.match(regexp));
     const res = username.substring(username.indexOf('=')).slice(1);
-    console.log(`Welcome to the File Manager, ${res}!`);
-    process.exit();
+    return res;
+}
+
+const exit = () => {
+    console.log(`Thank you for using File Manager, ${getUserName()}, goodbye!`);
+    process.exit(0);
+}
+
+const start = () => {
+    console.log(`Welcome to the File Manager, ${getUserName()}!`);
+    exit();
+}
+
+const init  = () => {
+    start();
 }
 
 init();
-
