407fd27f8a200717e01170e3c5e6f4f7d35e0941
UlianaSavi <uljasha.sav@gmail.com>
2023-06-12 16:33:45 +0600

    feat: add sort to ls fun

--- scripts/navigation/ls.js
+++ scripts/navigation/ls.js
@@ -3,7 +3,7 @@ import fs from 'fs/promises'
 export const showTable = async () => {
     try {
         const dir = process.cwd();
-        const array = await fs.readdir(dir);
+        const array = (await fs.readdir(dir)).sort();
         const files = array.filter((item) => item.includes('.'));
 
         const structDatas = [];
@@ -11,6 +11,7 @@ export const showTable = async () => {
         for (let i = 0; i < array.length; i++) {
             structDatas.push({ NAME: array[i], TYPE: array[i].includes(files[i]) ? 'file' : 'dir' });
         }
+        console.log('\n');
         console.table(structDatas);
       } catch (err) {
         throw new Error(err);
