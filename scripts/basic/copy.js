a8c0e2f5aaf018e125ce34dd9f51eade95199944
UlianaSavi <uljasha.sav@gmail.com>
2023-06-13 20:22:17 +0600

    refactor: change way to copy file

--- scripts/basic/copy.js
+++ scripts/basic/copy.js
@@ -1,10 +1,10 @@
-import { promises as fs } from 'fs';
+import fs from 'fs';
 
 export const copy = async (line = '') => {
     const oldPath = line.split(' ').at(0);
     const newPath = line.split(' ').at(1);
     try {
-        fs.copyFile(oldPath, newPath)
+        fs.createReadStream(oldPath).pipe(fs.createWriteStream(newPath));
     } catch (err) {
         console.log(err);
     }
